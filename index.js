39d1ad01ed6df544c5a2ebeebe42672a3fb409a1
jeevi <ramajeevitha58@gmail.com>
2025-10-04 02:17:44 +0530

    done

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const sampleData = require('./data/sample.json'); 
 const connectDB = require('./db_config.js'); 
 const User = require('./models/User.js');
@@ -6,24 +7,23 @@ const Room = require('./models/room.js');
 const userRoutes = require('./routes/users');
 const roomRoutes = require('./routes/rooms');
 
+require('dotenv').config();
 
-require('dotenv').config()
+const app = express();
 
-const app = express();   
 
+app.use(cors({ origin: "http://localhost:3002" }));
+
+app.use(express.json()); 
 
 connectDB();
-app.use(express.json()); 
 
 app.use('/api/users', userRoutes);
 app.use('/api/rooms', roomRoutes);
 
-
-
-
 app.get('/sample', (req, res) => {
-    res.json(sampleData); 
+  res.json(sampleData); 
 });
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3001;
 app.listen(port, () => console.log(`Server running on port ${port}`));
